2e867278436d34e488f8e5acf51450b68d505b58
emily <emilylibiz0707@gmail.com>
2024-05-25 07:06:59 -0700

    update blog style

--- pages/[slug].js
+++ pages/[slug].js
@@ -11,39 +11,40 @@ const client = new ApolloClient({
     cache: new InMemoryCache()
 });
 
-export default function Post({ post }) {
-    return (
-      <div className="container mx-auto flex flex-wrap pl-8">
-       {/* Main content */}
-        <div className="w-full lg:w-2/3 flex flex-col items-start px-1">
-          <div className="flex flex-col w-full mt-8">
-            <h1 className="text-4xl text-black font-bold leading-none lg:leading-snug mb-2 py-6 ">{post.title}</h1>
-            <div className = "border-t-2 ">
-              <p className="text-sm text-gray-500 py-2 pl-2">{new Date(post.createdAt).toLocaleDateString()}</p>
-              <div className="prose px-2">
-                  <MDXRemote {...post.content} />
-              </div>
-            </div>
 
-          </div>
-        </div>
-  
-        {/* Sidebar */}
-        <div className="w-full lg:w-1/3 flex flex-col items-center px-3">
-          <div className="w-full bg-white shadow flex flex-col my-4 p-6">
-            <p className="text-xl text-gray-800 font-bold border-b pb-2">关于我</p>
-            <p className="text-sm text-gray-600 mt-2">
-            嗨，欢迎来到我的博客！我叫Emily，是一个身兼数职的普通妈妈，既是孩子们依赖的妈咪，又是科技大厂写代码的软件工程师，更是自己小企业的掌舵人。我的生活就像一部多角色剧，每一天都在不同的角色间切换，迎接着新的挑战与机遇。
-            </p>
-            <Link href="/aboutme" className="text-sm  text-blue-600 hover:underline mt-4">了解更多</Link>
-          </div>
-          {/* <div className="w-full bg-white shadow flex flex-col my-4 p-6">
-            <p className="text-xl text-gray-800 font-bold border-b pb-2">推荐阅读</p>
-          </div> */}
+export default function Post({ post }) {
+  return (
+    <div className="container mx-auto flex flex-wrap pt-8 px-16 lg:px-0">
+      {/* Main Content */}
+      <div className="w-full lg:w-3/4 px-4 lg:pr-10">
+        <h1 className="text-3xl lg:text-4xl font-bold mb-6">{post.title}</h1>
+        <p className="text-gray-600 text-lg mb-4">{new Date(post.createdAt).toLocaleDateString()}</p>
+        <div className="prose lg:prose-lg max-w-none pb-16">
+          <MDXRemote {...post.content} />
         </div>
       </div>
-    );
-  }
+      
+      {/* Sidebar */}
+      <div className="w-full lg:w-1/4 lg:pl-4">
+        <div className="sticky top-20 p-6 bg-white shadow-lg rounded-lg bg-gray-100 p-4 rounded-lg shadow-inner">
+          <div className="text-center mb-6 ">
+            <img src="/emily.png" alt="Jaden" className="rounded-full w-32 h-32 mx-auto mb-4"/>
+            <h2 className="text-xl font-semibold">Hi! I'm Emily!</h2>
+            <p className="text-gray-600 text-sm ">欢迎来到我的博客</p>
+          </div>
+          <p className="text-gray-700 mb-4">
+            我是一个身兼数职的普通妈妈： 既是自己小企业的掌舵人，又是科技大厂写代码的软件工程师，还是孩子们依赖的妈咪，每一天都在不同的角色间切换，迎接着新的挑战与机遇。 
+          </p>
+          <div className="flex justify-center">
+              <a href="/aboutme" className="text-sm inline-block bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded shadow transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-base">
+                  了解更多
+              </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
   export async function getStaticPaths() {
     let slugs = [];
@@ -90,37 +91,6 @@ export async function getStaticProps({ params }) {
         }
     }
 }
-
-
-function convertContentToMarkdown1(blocks) {
-    return blocks.map(block => {
-      switch (block.type) {
-        case 'image' :
-          const imageUrl = block.image.formats.small.url.startsWith('http')
-            ? block.image.formats.small.url
-            : BACKEND_URL + block.image.formats.small.url;
-          const altText = block.image.alternativeText || 'image';
-          return `![${altText}](${imageUrl})`;
-        case 'paragraph':
-          return block.children.map(child => child.text).join('');
-        case 'link':  // Assuming 'link' is a type in your data blocks
-          const url = block.url.startsWith('http') ? block.url : BACKEND_URL + block.url;
-          const text = block.text || url; // Use URL as text if no text provided
-          return `[${text}](${url})`;
-        case 'list':
-          if (block.format === 'unordered') {
-            return block.children.map(item => `* ${item.children.map(child => child.text).join('')}`).join('\n');
-          } else {
-            // For ordered lists
-            return block.children.map((item, index) => `${index + 1}. ${item.children.map(child => child.text).join('')}`).join('\n');
-          }
-        // Add cases for other types of blocks if necessary
-        default:
-          return '';
-      }
-    }).join('\n\n');
-  }
-  
 
   function convertContentToMarkdown(blocks) {
     // Function to process text and nested elements like links
