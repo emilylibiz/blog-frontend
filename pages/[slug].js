30dc4b16af3b9d212456424000f0523c99c974cf
emily <emilylibiz0707@gmail.com>
2024-07-27 09:12:16 -0700

    add format for bold and new lines

--- pages/[slug].js
+++ pages/[slug].js
@@ -8,8 +8,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const client = new ApolloClient({
-    uri: GRAPHQL_API_URL,
-    cache: new InMemoryCache()
+  uri: GRAPHQL_API_URL,
+  cache: new InMemoryCache()
 });
 
 
@@ -24,7 +24,7 @@ export default function Post({ post }) {
           <MDXRemote {...post.content} />
         </div>
       </div>
-      
+
       {/* Sidebar */}
       <div className="w-full lg:w-1/4 lg:pl-4">
         <div className="sticky top-20 p-6 bg-white shadow-lg rounded-lg bg-gray-300 p-4 rounded-lg shadow-inner">
@@ -40,12 +40,12 @@ export default function Post({ post }) {
             <p className="text-gray-600 text-sm ">欢迎来到我的博客</p>
           </div>
           <p className="text-gray-700 mb-4">
-            我是一个身兼数职的普通妈妈： 既是自己小企业的掌舵人，又是科技大厂写代码的软件工程师，还是孩子们依赖的妈咪，每一天都在不同的角色间切换，迎接着新的挑战与机遇。 
+            我是一个身兼数职的普通妈妈： 既是自己小企业的掌舵人，又是科技大厂写代码的软件工程师，还是孩子们依赖的妈咪，每一天都在不同的角色间切换，迎接着新的挑战与机遇。
           </p>
           <div className="flex justify-center">
-              <Link href="/aboutme" className="text-sm inline-block bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded shadow transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-base">
-                  了解更多
-              </Link>
+            <Link href="/aboutme" className="text-sm inline-block bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded shadow transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-base">
+              了解更多
+            </Link>
           </div>
         </div>
       </div>
@@ -53,86 +53,92 @@ export default function Post({ post }) {
   );
 }
 
-  export async function getStaticPaths() {
-    let slugs = [];
-    let page = 1;
-    let totalPages = 1;
-  
-    do {
-      const { data } = await client.query({
-        query: GET_ALL_SLUGS,
-        variables: {
-          start: (page - 1) * 10,  // assuming the default limit is 10
-          limit: 10
-        }
-      });
-      
-      slugs = slugs.concat(data.blogPosts.data.map(post => ({
-        params: { slug: post.attributes.urlSlug }
-      })));
-  
-      totalPages = Math.ceil(data.blogPosts.meta.pagination.total / 10); // Adjust based on actual meta data
-      page++;
-    } while (page <= totalPages);
-  
-    return {
-      paths: slugs,
-      fallback: false
-    };
-  }
-export async function getStaticProps({ params }) {
+export async function getStaticPaths() {
+  let slugs = [];
+  let page = 1;
+  let totalPages = 1;
+
+  do {
     const { data } = await client.query({
-        query: GET_INDIVIDUAL_POST,
-        variables: { slugUrl: params.slug }
+      query: GET_ALL_SLUGS,
+      variables: {
+        start: (page - 1) * 10,  // assuming the default limit is 10
+        limit: 10
+      }
     });
 
-    const attrs = data.blogPosts.data[0].attributes;
-    const html = await serialize(convertContentToMarkdown(attrs.content));
-    return {
-        props: {
-            post: {
-                title: attrs.title,
-                createdAt: attrs.createdAt,
-                content: html,
-            }
-        }
+    slugs = slugs.concat(data.blogPosts.data.map(post => ({
+      params: { slug: post.attributes.urlSlug }
+    })));
+
+    totalPages = Math.ceil(data.blogPosts.meta.pagination.total / 10); // Adjust based on actual meta data
+    page++;
+  } while (page <= totalPages);
+
+  return {
+    paths: slugs,
+    fallback: false
+  };
+}
+export async function getStaticProps({ params }) {
+  const { data } = await client.query({
+    query: GET_INDIVIDUAL_POST,
+    variables: { slugUrl: params.slug }
+  });
+
+  const attrs = data.blogPosts.data[0].attributes;
+  const html = await serialize(convertContentToMarkdown(attrs.content));
+  return {
+    props: {
+      post: {
+        title: attrs.title,
+        createdAt: attrs.createdAt,
+        content: html,
+      }
     }
+  }
 }
 
-  function convertContentToMarkdown(blocks) {
-    // Function to process text and nested elements like links
-    const processTextElements = (elements) => {
-        return elements.map(element => {
-            if (element.type === 'text') {
-                return element.text;
-            } else if (element.type === 'link') {
-              const linkText = element.children.map(child => child.text).join('');
-              // Format it as a markdown link
-              return `[${linkText}](${element.url})`;
-            }
-            return ''; // Return an empty string for unsupported types
-        }).join('');
-    };
+function convertContentToMarkdown(blocks) {
+  // Function to process text and nested elements like links
+  const processTextElements = (elements) => {
+    return elements.map(element => {
+      if (element.type === 'text') {
+        return element.text;
+      } else if (element.type === 'link') {
+        const linkText = element.children.map(child => child.text).join('');
+        // Format it as a markdown link
+        return `[${linkText}](${element.url})`;
+      }else if (element.type === 'bold') {
+        const boldText = element.children.map(child => child.text).join('');
+        // Format it as a markdown bold
+        return `**${boldText}**`;
+      } else if (element.type === 'blank_line') {
+        return '\n\n'; // Add blank lines in markdown
+      }
+      return ''; // Return an empty string for unsupported types
+    }).join('');
+  };
 
-    return blocks.map(block => {
-        switch (block.type) {
-            case 'paragraph':
-                return processTextElements(block.children);
-            case 'image':
-                const imageUrl = block.image.url.startsWith('http')
-                    ? block.image.url
-                    : BACKEND_URL + block.image.url;
-                const altText = block.image.alternativeText || 'image';
-                return `![${altText}](${imageUrl})`;
-            case 'list':
-                if (block.format === 'unordered') {
-                    return block.children.map(item => `* ${processTextElements(item.children)}`).join('\n');
-                } else {
-                    // For ordered lists
-                    return block.children.map((item, index) => `${index + 1}. ${processTextElements(item.children)}`).join('\n');
-                }
-            default:
-                return ''; // Handle other types similarly if needed
+  return blocks.map(block => {
+    switch (block.type) {
+      case 'paragraph':
+        return processTextElements(block.children);
+      case 'image':
+        const imageUrl = block.image.url.startsWith('http')
+          ? block.image.url
+          : BACKEND_URL + block.image.url;
+        const altText = block.image.alternativeText || 'image';
+        return `![${altText}](${imageUrl})`;
+      case 'list':
+        if (block.format === 'unordered') {
+          return block.children.map(item => `* ${processTextElements(item.children)}`).join('\n');
+        } else {
+          // For ordered lists
+          return block.children.map((item, index) => `${index + 1}. ${processTextElements(item.children)}`).join('\n');
         }
-    }).join('\n\n');
+      default:
+        return ''; // Handle other types similarly if needed
+    }
+  }).join('\n\n');
 }
